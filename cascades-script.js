5fa9a01e192ba8d352c2f59b49e600081c134eb6
Christina Kiesz <ckiesz@gmail.com>
2024-11-12 13:02:33 -0800

    createBoard

---
+++ cascades-script.js
@@ -0,0 +1,42 @@
+// 1. create a board with random tiles
+const width = 8;
+const height = 8;
+const gameBoard = document.getElementById("game-board");
+let squares = [];
+let colors = ['1','2','3','4','5','6','7']
+// board pattern
+//  0  1  2  3  4  5  6  7
+//  8  9 10 11 12 13 14 15
+// 16 17 18 19 20 21 22 23
+// 24 25 26 27 28 29 30 31 ...
+
+function createBoard() {
+	// iterate width by height to fill the board
+	for (let i = 0; i < width*height; i++) {
+		// create a new div
+		let square = document.createElement('div');
+		// give it an id
+		square.id = i;
+		// add to it the tile class
+		square.classList.add('tile');
+		// pick a random color/dataset-type by indexing into the colors array
+		let randomColor = colors[Math.floor(Math.random() * colors.length)]
+		square.dataset.type = randomColor;
+		gameBoard.appendChild(square);
+		squares.push(square);
+
+	}
+}
+
+// isThreeInARow should check for 3 in a row horz and vert
+// returns true if the current index and color would form 3
+function  isThreeInARow(index, colorType){
+	// horizontal check can be simplified to only check what is to the left of it
+	// allows skipping the first two indexes on the left for each row
+	
+
+	// vertical check can be simplified to only check above it
+	// allows skipping the first two rows of indexes for each column
+}
+
+createBoard();
