af4977cdcbc01f50ae832b70051fbd0b865ef82f
Christina Kiesz <ckiesz@gmail.com>
2024-11-12 16:03:05 -0800

    can switch any two squares on the grid

--- cascades-script.js
+++ cascades-script.js
@@ -4,7 +4,7 @@ const width = 8;
 const height = 10;
 const gameBoard = document.getElementById("game-board");
 let squares = [];
-let colors = ['1','2','3','4','5','6','7']
+let colors = ['1', '2', '3', '4', '5', '6', '7']
 // board pattern
 //  0  1  2  3  4  5  6  7
 //  8  9 10 11 12 13 14 15
@@ -13,7 +13,7 @@ let colors = ['1','2','3','4','5','6','7']
 
 function createBoard() {
 	// iterate width by height to fill the board
-	for (let i = 0; i < width*height; i++) {
+	for (let i = 0; i < width * height; i++) {
 		// create a new div
 		let square = document.createElement('div');
 		// give it an id
@@ -30,6 +30,18 @@ function createBoard() {
 			square.dataset.type = randomColor;
 		} while (isThreeInARow(i, randomColor));
 
+		// add event listeners here for better efficiency
+		square.addEventListener('dragstart', dragStart);
+
+		// enter and over need to prevent default so we can allow drop target
+		square.addEventListener('dragenter', dragEnter);
+		square.addEventListener('dragover', (e) => e.preventDefault());
+
+		// dragend is when the mouse lets go
+		square.addEventListener('dragend', dragEnd);
+		// drop occurs only if a valid target
+		square.addEventListener('drop', dragDrop);
+
 		gameBoard.appendChild(square);
 		squares.push(square);
 
@@ -38,7 +50,7 @@ function createBoard() {
 
 // isThreeInARow should check for 3 in a row horz and vert
 // returns true if the current index and color would form 3
-function  isThreeInARow(index, colorType){
+function isThreeInARow(index, colorType) {
 	// horizontal check can be simplified to only check what is to the left of it
 	// allows skipping the first two indexes on the left for each row
 	// index modulo width is a way to see where it sits within the row
@@ -54,5 +66,46 @@ function  isThreeInARow(index, colorType){
 
 	return false;
 }
+
+let colorTypeBeingDragged, squareIdBeingDragged;
+function dragStart(e) {
+	colorTypeBeingDragged = this.dataset.type;
+	squareIdBeingDragged = parseInt(this.id);
+}
+
+function dragEnter(e) {
+	e.preventDefault()
+	colorTypeBeingReplaced = this.dataset.type;
+	squareIdBeingReplaced = parseInt(this.id);
+	// only a valid move if one in each direction
+	// edge cases are bottom, top, left, right outside of grid
+
+	// const validMoves = [
+	// 	squareIdBeingDragged - 1,
+	// 	squareIdBeingDragged + 1,
+	// 	squareIdBeingDragged - width,
+	// 	squareIdBeingDragged + width
+	// ];
+
+	// const validMove = validMoves.includes(squareIdBeingReplaced);
+}
+
+
+let colorTypeBeingReplaced, squareIdBeingReplaced;
+// dragend fires whenever let go of mouse
+function dragEnd(e) {
+	// colorTypeBeingReplaced = this.dataset.type;
+	// squareIdBeingReplaced = parseInt(this.id);
+	// console.log("color being replaced: ", colorTypeBeingReplaced)
+}
+
+// drop fires only if valid drop target under mouse on end
+function dragDrop(e) {
+	console.log("dragdrop fired");
+	squares[squareIdBeingReplaced].dataset.type = colorTypeBeingDragged;
+	squares[squareIdBeingDragged].dataset.type = colorTypeBeingReplaced;
+}
+
+
 
 createBoard();
