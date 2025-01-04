4fc5ec9e401840b8eb54ff9b24ddf396ddbbc863
Christina Kiesz <ckiesz@gmail.com>
2025-01-03 18:15:44 -0800

    board can display properly

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const __dirname = path.dirname(__filename);
 
 // Serve static files (HTML, CSS, JS)
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/src', express.static(path.join(__dirname, 'src')));
 
 // Default route
 app.get('/', (req, res) => {
---
+++ src/CascadesGame.js
@@ -0,0 +1,13 @@
+import Board from "./Board.js";
+class CascadesGame {
+	constructor() {
+		// board
+		this.board = new Board();
+		// score
+		this.score = 0;
+	}
+
+	
+}
+
+export default CascadesGame;
